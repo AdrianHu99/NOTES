2767934cf5a93b06a6c45301c2b75fa70aa25df0
Adrian Hu <qihu1992@hotmail.com>
2018-03-01 20:54:49 -0500

    Finished lesson1

--- OnlineCourses/Developing Android Apps/ud851-Exercises-student/Lesson01-Favorite-Toys/T01.03-Exercise-AddScrolling/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ OnlineCourses/Developing Android Apps/ud851-Exercises-student/Lesson01-Favorite-Toys/T01.03-Exercise-AddScrolling/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mToysListTextView;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
